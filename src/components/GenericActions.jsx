eddb170084071dc69697b26ff5e75069363d59e4
Devin Vincent <vincentdevin111@gmail.com>
2024-10-13 20:03:41 -0500

    added some icons and cleaned up styles

--- src/components/GenericActions.jsx
+++ src/components/GenericActions.jsx
@@ -19,7 +19,7 @@ const GenericActions = ({
                 <h2>{title}</h2> {/* Use the title prop here */}
                 <Link to={addLink} aria-label={`Add new ${title}`}>
                     <button className="btn btn-primary" onClick={onAdd}>
-                        Add {title.slice(0, -1)} {/* Singular form */}
+                        <i className="bi bi-plus-circle-fill"></i>
                     </button>
                 </Link>
             </div>
